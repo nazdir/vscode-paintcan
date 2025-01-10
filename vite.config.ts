0395812f74550cd5a23fa89fcd442356e7140788
Richard <richardjarrell@yahoo.com>
2025-01-10 12:39:07 -0600

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
-import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  build: {
-    outDir: 'build',
-  },
-  // server: {  },
-  plugins: [react(), viteTsconfigPaths(), svgr({ svgrOptions: { icon: true } })],
+  plugins: [react()],
 })
